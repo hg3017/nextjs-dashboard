e1ce2413e0dab5af75a0695e4456f9d176f8b20c
hg3017 <hyongyu1991@naver.com>
2024-11-08 16:46:01 +0900

    수정본

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,11 @@ import Pagination from '@/app/ui/invoices/pagination';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 // type Props = {
 //   searchParams?: Promise<{ query?: string; page?: string }>;
